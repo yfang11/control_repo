d134c322f02b902510392be27be5ab34167188c3
yfang11 <58044585+yfang11@users.noreply.github.com>
2020-04-22 16:09:37 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,7 +4,8 @@ class minecraft {
   }
   file { '/opt/minecraft/minecraft_server.jar':
     ensure => file,
-    source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+    source => 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+    before => Service['minecraft'],
   }
   package { 'java':
     ensure => present,
