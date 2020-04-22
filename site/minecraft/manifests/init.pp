cbf8313da5a99c877662907e67c87bdd710e90f4
yfang11 <58044585+yfang11@users.noreply.github.com>
2020-04-22 15:41:35 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file { '/opt/minecraft':
     ensure => directory,
   }
-  file { '/opt/minecraft/minecraft_server.jar':
+  file { '/opt/minecraft/minecraft_server.1.12.2.jar':
     ensure => file,
     source => 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
   }
