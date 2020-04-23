0c6e439c2b802772c03335c69e350505dffe6316
yfang11 <58044585+yfang11@users.noreply.github.com>
2020-04-23 08:59:33 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,6 @@
 class minecraft (
-  $url = 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
+  $url = 'https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar',
+  # 'https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar',
   $install_dir = '/opt/minecraft',
 ) {
   file { "${install_dir}":
